10795a36719d501ffeaeb419c286c6b6a99e6afd
Mazharul Juniad <juniadmazharul@gmail.com>
2026-09-08 16:47:24 -0400

    Update database configuration variables for directory

--- directory.php
+++ directory.php
@@ -9,10 +9,10 @@ $success_message = '';
 $entries_to_display = [];
 
 // --- Database Configuration ---
-$host = $env['DB_HOST'];
-$db   = $env['DB_NAME'];
-$user = $env['DB_USER'];
-$pass = $env['DB_PASS']; 
+$host = $env['DIRECTORY_DB_HOST'];
+$db   = $env['DIRECTORY_DB_NAME'];
+$user = $env['DIRECTORY_DB_USER'];
+$pass = $env['DIRECTORY_DB_PASS'];
 $charset = 'utf8mb4';
 $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
 $options = [
